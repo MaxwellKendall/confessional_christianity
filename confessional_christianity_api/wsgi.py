f734aceb82dfa601bdad0a612a58db1325e1bd5d
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2019-08-01 09:24:56 -0400

    first commit

---
+++ confessional_christianity_api/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for confessional_christianity_api project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'confessional_christianity_api.settings')
+
+application = get_wsgi_application()
