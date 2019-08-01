3bc7fe73ada56f4e41d8d1882981e54f413f0e2e
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2019-08-01 10:47:29 -0400

    [wcf] new app "wcf"

---
+++ confessional_christianity_api/data/test.py
@@ -0,0 +1,3 @@
+# assumes cwd is confessional_christianity
+wcf = open("confessional_christianity_api/data/WCF.txt")
+print(wcf.read())
---
+++ wcf/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class WcfConfig(AppConfig):
+    name = 'wcf'
