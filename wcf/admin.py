10434470b7c403530292050a025d172c5cd816ea
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2019-08-01 11:29:51 -0400

    [wcf] creates table wcf_wcf

--- wcf/admin.py
+++ wcf/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from .models import wcf
+
 # Register your models here.
+admin.site.register(wcf)
--- wcf/models.py
+++ wcf/models.py
@@ -1,3 +1,20 @@
-from django.db import models
+import datetime
 
-# Create your models here.
+from django.db import models
+from django.contrib.postgres.fields import JSONField, ArrayField
+
+# Create your models here. Class is a subclass of django.db.models.Model
+class wcf(models.Model):
+    def __str__(self):
+        return self.title
+    id = models.CharField(primary_key=True, max_length=1000) 
+    chapter_number = models.IntegerField()
+    title = models.CharField(max_length=1000)
+    # map: proof_reference_from_paragraph: proof
+    proofs = JSONField()
+    # array
+    paragraphs = ArrayField(models.TextField())
+
+    def get_details(self):
+        # Do something here in the future?
+        return self.title + self.chapter_number
