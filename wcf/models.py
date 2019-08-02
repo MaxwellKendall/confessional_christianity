8c680ea4699df4e95b59d6a4a6417a784d33670b
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2019-08-02 15:45:37 -0400

    wcf/chapter# api request

--- wcf/models.py
+++ wcf/models.py
@@ -7,14 +7,18 @@ from django.contrib.postgres.fields import JSONField, ArrayField
 class wcf(models.Model):
     def __str__(self):
         return self.title
+
     id = models.CharField(primary_key=True, max_length=1000) 
     chapter_number = models.IntegerField()
     title = models.CharField(max_length=1000)
-    # map: proof_reference_from_paragraph: proof
     proofs = JSONField()
-    # array
     paragraphs = ArrayField(models.TextField())
 
-    def get_details(self):
+    def get_all_data(self):
         # Do something here in the future?
-        return self.title + self.chapter_number
+        return {
+            'title': self.title,
+            'paragraphs': self.paragraphs,
+            'proofs': self.proofs,
+            'chapter_number': self.chapter_number
+        }
--- wcf/views.py
+++ wcf/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+
+from .models import wcf
 
 # Create your views here.
+
+def index(request, chapter):
+    requestedChapter = "WCF_" + str(chapter)
+    chapter = wcf.objects.get(id=requestedChapter)
+    return HttpResponse(JsonResponse(chapter.get_all_data()))
