d78d9599064148064166d11797d4798b6c5d083e
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2019-08-02 11:45:37 -0400

    removing prepended number in each paragraph

--- wcf/management/commands/import_wcf.py
+++ wcf/management/commands/import_wcf.py
@@ -4,20 +4,23 @@ from django.core.management.base import BaseCommand
 from wcf.models import wcf
 
 class Command(BaseCommand):
-    help = 'Populates the DB with the Westminster Confession of Faith'
+    help = 'Populates the DB with the Westminster Confession of Faith :bang!:'
+    def remove_numbers(self, char):
+        return char != "1." and char != "2." and char != "3." and char != "4." and char != "5." and char != "6." and char != "7." and char != "8." and char != "9." and char != "10." and char != "11." and char != "12." and char != "13." and char != "14." and char != "15." and char != "16."
     def parse_title(self, arrayOfTitleWords):
         return ' '.join(arrayOfTitleWords).strip()
     def parse_paragraphs(self, arrayOfParagraphs):
-        rtrn = []
+        paragraphsForChapter = []
         for paragraph in arrayOfParagraphs:
-            arrayOfWordsInParagraph = list(filter(lambda item: item != '__WCF_SCRIPTURE_REF', paragraph.split(' ')))
-            rtrn.append(' '.join(arrayOfWordsInParagraph))
-        return rtrn
+            arrayOfWordsInParagraph = list(
+                filter(lambda item: item != '__WCF_SCRIPTURE_REF' and self.remove_numbers(item), paragraph.strip().split(' '))
+            )
+            paragraphsForChapter.append(' '.join(arrayOfWordsInParagraph).strip())
+        return paragraphsForChapter
     def parse_proof(self, data):
         self.stdout.write(data)
     def build_chapter(self, data):
         # firstParagraph is right after end of the Chapter title
-        print("DATA *****", data)
         firstParagraphIndex = data.index('__WCF_PARAGRAPH__')
         firstProofIndex = data.index('WCF_PROOF') - 1
         chapterAsString = ' '.join(data[firstParagraphIndex:firstProofIndex]).strip()
@@ -28,19 +31,18 @@ class Command(BaseCommand):
             'proofs': 'blah blah',
             'id': 'WCF_123'
         }
-    def write_to_db(self, chapter):
-        print("eventually will save this to the db", chapter)
+    def write_to_db(self, wcf):
+        print(wcf[30]['paragraphs'][0])
 
     def handle(self, *args, **options):
-        rtrn = []
-        # import logic goes here
+        arrayOfWcfChapters = []
         wcf = open("confessional_christianity_api/data/WCF.txt").read().split('__WCF_CHAPTER__')
         for index, chapter in enumerate(wcf[1:]):
             print("CHAPTER", index)
             obj = self.build_chapter(chapter.replace('\n', '').split(' '))
             obj['id'] = 'WCF_' + str(index + 1)
-            rtrn.append(obj)
-        print(rtrn[30])
+            arrayOfWcfChapters.append(obj)
+        self.write_to_db(arrayOfWcfChapters)
         self.stdout.write(self.style.SUCCESS('Success!!'))
 
 # https://docs.djangoproject.com/en/dev/howto/custom-management-commands/ & https://eli.thegreenplace.net/2014/02/15/programmatically-populating-a-django-database
