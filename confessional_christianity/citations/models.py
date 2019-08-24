cc57eecfc8259d7a55daa35391206b8ff1aedd71
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2019-08-24 14:49:31 -0400

    [refactoringModel] successfully running migrations

--- confessional_christianity/citations/models.py
+++ confessional_christianity/citations/models.py
@@ -3,21 +3,17 @@ import datetime
 from django.db import models
 from django.contrib.postgres.fields import JSONField, ArrayField
 
+from confessions.models import Confessions, Headings, Passages
+
 # Create your models here. Class is a subclass of django.db.models.Model
-class citations(models.Model):
+class Citations(models.Model):
     def __str__(self):
-        return self.title
+        return self.passageId + self.tags
 
-    id = models.CharField(primary_key=True, max_length=1000) 
-    chapter_number = models.IntegerField()
-    title = models.CharField(max_length=1000)
-    proofs = JSONField()
-    paragraphs = ArrayField(models.TextField())
-
-    def get_all_data(self):
-        return {
-            'title': self.title,
-            'paragraphs': self.paragraphs,
-            'proofs': self.proofs,
-            'chapter_number': self.chapter_number
-        }
+    id = models.CharField(primary_key=True, max_length=1000)
+    confession = models.ForeignKey(Confessions, on_delete=models.CASCADE)
+    heading = models.ForeignKey(Headings, on_delete=models.CASCADE)
+    passage = models.ForeignKey(Passages, on_delete=models.CASCADE)
+    referenceIdentifier = JSONField()
+    scripture = ArrayField(models.TextField())
+    tags = ArrayField(models.TextField())
